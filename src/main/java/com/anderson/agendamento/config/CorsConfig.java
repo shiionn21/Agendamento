c7cf5f92efb8b0646a3972e1a7300597d38abfaa
Anderson Souza <anderson.souza8522@gmail.com>
2026-08-28 16:43:32 -0300

    Adiciona tela (frontend) e CORS

--- src/main/java/com/anderson/agendamento/config/CorsConfig.java
+++ src/main/java/com/anderson/agendamento/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.anderson.agendamento.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
